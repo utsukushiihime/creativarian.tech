92e8d4df20655e7a256633697eb8d5b0bab7147c
creattech <crystalsmcneil@gmail.com>
2020-10-03 21:42:06 -0500

    init commit

---
+++ pages/blog-details.js
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
+import PageHeader from "../components/PageHeader";
+import BlogDetails from "../components/BlogDetails";
+import Footer from "../components/Footer";
+
+const BlogDetailPage = () => (
+  <Layout pageTitle="Creativarian Tech - Blog Details">
+    <Navbar />
+    <PageHeader title="Blog Details" />
+    <BlogDetails />
+    <Footer />
+  </Layout>
+);
+
+export default BlogDetailPage;
